915e998cec3b17b80ad663d81862a25a25c35fee
ankm19 <ankm335@gmail.com>
2023-03-21 16:20:56 +0530

    learning 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Dashboard from './components/Dashboard'
+import Navbar from './components/Navbar'
+// import { Button, Typography } from '@mui/material'
+// import './App.css'
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <Navbar/>
+      </div>
+    </div>
+  )
+}
+
+export default App
